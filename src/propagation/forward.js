5a4a2faa4535bad5cdfbdddd85ce586d7456dd37
Everth Sarantes <everthleonardosaran@gmail.com>
2026-06-05 17:49:08 -0600

    add forward propagation method

---
+++ src/propagation/forward.js
@@ -0,0 +1,71 @@
+import { getActivation } from '../activation_methods/registry.js';
+
+/**
+ * Ejecuta la propagación hacia adelante (Forward Pass) en todo el grafo de la red.
+ * @param {Network} network - La instancia maestra de la red.
+ * @param {Array<number>} inputVector - Array con los valores de entrada de una fila del CSV.
+ * @returns {Array<number>} Los valores de activación finales de la capa de salida.
+ */
+export function forwardPass(network, inputVector) {
+    if (network.layers.length === 0) return [];
+
+    // INYECTAR ENTRADAS
+    const inputLayer = network.layers[0];
+    inputLayer.neurons.forEach((neuron, idx) => {
+        neuron.value = inputVector[idx] || 0;
+        neuron.netInput = neuron.value;
+        neuron.isDropped = false;
+    });
+
+    // APLICAR MÁSCARA DE DROPOUT
+    if (network.isTraining && network.dropoutRate > 0) {
+        for (let i = 1; i < network.layers.length - 1; i++) {
+            const layer = network.layers[i];
+            layer.neurons.forEach(neuron => {
+                neuron.isDropped = Math.random() < network.dropoutRate;
+            });
+        }
+    } else {
+        network.layers.forEach(layer => {
+            layer.neurons.forEach(n => n.isDropped = false);
+        });
+    }
+
+    // PROPAGACIÓN CAPA POR CAPA
+    for (let i = 1; i < network.layers.length; i++) {
+        const layer = network.layers[i];
+        const activationMethod = getActivation(layer.activationId);
+
+        layer.neurons.forEach(neuron => {
+            if (neuron.isDropped) {
+                neuron.netInput = 0;
+                neuron.value = 0;
+                return;
+            }
+
+            // Ecuación de la Entrada Neta: netInput = Suma(X_i * W_i) + b
+            let sum = 0;
+            neuron.inputs.forEach(connection => {
+                if (!connection.from.isDropped && !connection.isDropped) {
+                    sum += connection.from.value * connection.weight;
+                }
+            });
+
+            neuron.netInput = sum + neuron.bias;
+
+            let activationValue = activationMethod.calculate(neuron.netInput);
+
+            // Ajuste de Inverted Dropout:
+            // Escalamos las neuronas ocultas sobrevivientes para compensar
+            if (network.isTraining && network.dropoutRate > 0 && layer.type === 'hidden') {
+                activationValue /= (1 - network.dropoutRate);
+            }
+
+            neuron.value = activationValue;
+        });
+    }
+
+    // RECOLECTAR RESULTADOS DE SALIDA
+    const outputLayer = network.layers[network.layers.length - 1];
+    return outputLayer.neurons.map(n => n.value);
+}
